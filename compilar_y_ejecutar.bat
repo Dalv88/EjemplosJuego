c0bccf230d17606599ca2bedb7f4611fbd38dae7
mauririo rodrigues <a24310388@live.ceti.mx>
2025-12-13 06:11:05 -0600

    cambio de nombre

--- compilar_y_ejecutar.bat
+++ compilar_y_ejecutar.bat
@@ -1,14 +1,14 @@
 @echo off
 echo Cerrando procesos anteriores del juego...
-taskkill /F /IM 09_GalagaInvertido.exe 2>nul
+taskkill /F /IM JuegoProyecto.exe 2>nul
 
 echo Compilando el juego...
-g++ src/09_GalagaInvertido.cpp -o bin/09_GalagaInvertido.exe -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lbox2d -Iinclude
+g++ src/JuegoProyecto.cpp -o bin/JuegoProyecto.exe -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lbox2d -Iinclude
 
 if %ERRORLEVEL% EQU 0 (
     echo Compilacion exitosa!
     echo Ejecutando el juego...
-    bin\09_GalagaInvertido.exe
+    bin\JuegoProyecto.exe
 ) else (
     echo Error en la compilacion.
     pause
