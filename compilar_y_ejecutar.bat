af80150371f09c27af65a33150575e9a8256c77e
mauririo rodrigues <a24310388@live.ceti.mx>
2025-12-13 06:26:21 -0600

    name change

--- compilar_y_ejecutar.bat
+++ compilar_y_ejecutar.bat
@@ -1,14 +1,14 @@
 @echo off
 echo Cerrando procesos anteriores del juego...
-taskkill /F /IM JuegoProyecto.exe 2>nul
+taskkill /F /IM JuegoProyecto252.exe 2>nul
 
 echo Compilando el juego...
-g++ src/JuegoProyecto.cpp -o bin/JuegoProyecto.exe -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lbox2d -Iinclude
+g++ src/JuegoProyecto252.cpp -o bin/JuegoProyecto252.exe -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lbox2d -Iinclude
 
 if %ERRORLEVEL% EQU 0 (
     echo Compilacion exitosa!
     echo Ejecutando el juego...
-    bin\JuegoProyecto.exe
+    bin\JuegoProyecto252.exe
 ) else (
     echo Error en la compilacion.
     pause
